a36a91280cb1db5438d9e3ae82fff9b6b3905edf
imsia <irynasklyarova@gmail.com>
2016-05-18 02:16:07 +0300

    Update script.js

--- JS11-12/js/script.js
+++ JS11-12/js/script.js
@@ -6,7 +6,6 @@ $(function () {
 
   var data = {
     name: "Склярова Ирина Андреевна",
-    photoURL: "'../img/photo.JPG'",
     profession: "Студентка курса GoFrontend GoForIt Online",
     reason1: "Хочу научиться новому и полезному",
     reason2: "Хочу сменить профессию на более востребованную и прибыльную",
