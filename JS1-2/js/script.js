3e411ee4a245f59f9227157ead87fbbeb924f14f
imsia <irynasklyarova@gmail.com>
2016-05-18 01:08:41 +0300

    Update script.js

--- JS1-2/js/script.js
+++ JS1-2/js/script.js
@@ -21,25 +21,22 @@ function pow(a, n) {
 
 //Поиск имени в массиве
 
-var arrayName = [];
-var n = 5;
-var flag = 0;
-for (var i = 0; i < n; i++) {
-  arrayName[i] = prompt ('Введите имя пользователя - ');
+var arrUsers = [];
+for ( var i = 0; i < 2; i++) {
+  arrUsers[i] = prompt('Введите ' + ( i + 1 ) + '-е имя пользователя', 'Константин');
 }
-// console.log ('array ',  arrayName);
-var userName = prompt ('Введите имя пользователя - ');
-// console.log ('userName ', userName);
-for (var i = 0; i < arrayName.length; i++) {
-  if (userName == arrayName[i]) {
-    flag++;
+var currentUser = prompt('Добро пожаловать! Введите имя пользователя', 'Светлана');
+var allowToLogin = false;
+for ( var i = 0; i <= arrUsers.length; i++) {
+  if ( currentUser === arrUsers[i] ) {
+    allowToLogin = true;
   }
 }
-// console.log ('array length ', arrayName.length, '\nflag ', flag);
-if (flag) {
-  alert (userName + ' Вы успешно вошли!');
-} else {
-    alert ('Ошибка');
-}
+
+if (allowToLogin) {
+    alert( currentUser + ', Вы успешно вошли!' );
+  } else {
+    alert( 'Ошибка! Пользователь не найден!' )
+  }
 
 
